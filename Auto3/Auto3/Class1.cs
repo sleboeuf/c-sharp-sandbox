68766ca00457b4a1d73440cfce9f0d26b0830d15
Steven LeBoeuf <stevenleboeuf@radiusww.com>
2019-11-27 22:35:55 -0500

    Test VS

---
+++ Auto3/Auto3/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto3
+{
+    public class Class1
+
+    {
+        int x = 4;
+    }
+}

82930eff3f4c82326af5748d2122f0cfc94030a7
Steven LeBoeuf <stevenleboeuf@radiusww.com>
2019-12-06 15:01:28 -0500

    Adding NUnit example

---
+++ Auto3/Auto3/Driver.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto3
+{
+    public class Driver
+    {
+        private IWebDriver driver;
+
+        //Properties
+        public int TimeOut 
+        { get; set; }
+
+        public int Retries
+        { get; set; }
+
+        public double Delay
+        { get; set; }
+
+        public int PageLoadTimeout
+        { get; set; }
+        
+        //Constructors
+        public Driver(string browser)
+        {
+            TimeOut = 90;
+            Retries = 3;
+            Delay = 0.3;
+            PageLoadTimeout = 240;
+
+            if( browser == "Chrome")
+            {
+                driver = new ChromeDriver();
+            }
+
+
+        }
+
+        public Driver() : this("Chrome")
+        {
+        }
+
+        //Methods
+
+        public bool NavigateToURL(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+
+            return true;
+        }
+
+        public void CloseBrowser() 
+        {
+            driver.Close();
+        }
+
+    }
+}
---
+++ Auto3/Auto3/NUnitExample.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto3
+{
+    class NUnitExample
+    {
+        IWebDriver driver;
+        Driver myDriver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            myDriver = new Driver();
+            //driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void test()
+        {
+            myDriver.NavigateToURL("http://www.facebook.com");
+            //driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            myDriver.CloseBrowser();
+            //driver.Close();
+        }
+
+    }
+}
